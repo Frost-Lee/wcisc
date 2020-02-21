ec7c848d2a1ee9e0b54b86dd258c69a934ddb324
Frost Lee <canchen.lee@gmail.com>
2020-02-21 15:01:23 -0500

    userdefaults initialization.

--- wcisc/wcisc/AppDelegate.swift
+++ wcisc/wcisc/AppDelegate.swift
@@ -15,7 +15,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        UserDefaults.standard.register(defaults: [
+            "deviceName": "devicename"
+        ])
         return true
     }
 
